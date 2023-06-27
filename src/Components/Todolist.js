b99ae5727a7d5f78cc1b433e37b33ca82a806c61
Akkala Sai Mukesh Kumar <126863891+Mukeshdiksha2018@users.noreply.github.com>
2023-06-28 01:17:03 +0530

    made few styling changes on table data

--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -6,19 +6,20 @@ class Todolist extends Component {
         return (
             <React.Fragment>
                 <tr>
-                    <td style={{width:15}} className='text-center'>
+                    <td style={{width:200}} className='text-center'>
                         {this.props.number}
                     </td>
-                    <td style={{width:15}} className='text-center'>
+                    <td style={{width:200}} className='text-center'>
                         <input type='checkbox'/>
                     </td>
-                    <td style={{width:200}} className='text-center'>
+                    <td style={{width:500}} className='text-center'>
                         {this.props.myToDo.value}
                     </td>
-                    <td style={{width:50}} className='text-center'>
+                    <td style={{width:500}} className='text-center'>
                         <button onClick={()=>{this.props.myDelete(this.props.myToDo)}} className='btn btn-danger'>Delete</button>
                     </td>
                 </tr>
+               
             </React.Fragment>
         );
     }
